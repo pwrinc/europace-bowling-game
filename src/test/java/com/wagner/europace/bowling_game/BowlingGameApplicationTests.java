6190028beba35550b67715bb7695c0c827aaf6ef
Philipp Wagner <wagner@getrequest.de>
2024-12-19 16:19:35 +0100

    add another test case: maximum score without a strike

--- src/test/java/com/wagner/europace/bowling_game/BowlingGameApplicationTests.java
+++ src/test/java/com/wagner/europace/bowling_game/BowlingGameApplicationTests.java
@@ -20,6 +20,7 @@ class BowlingGameApplicationTests {
 		ScoreCalculator calculator = new ScoreCalculator();
 		assertEquals("Calculated score is 133", calculator.calc("1,4", "4,5", "6,4", "5,5", "10,0", "0,1", "7,3", "6,4", "10,0", "2,8,6"));
 		assertEquals("Calculated score is 300", calculator.calc("10,0", "10,0", "10,0", "10,0", "10,0", "10,0", "10,0", "10,0", "10,0", "10,10,10"));
+		assertEquals("Calculated score is 190", calculator.calc("9,1", "9,1", "9,1", "9,1", "9,1", "9,1", "9,1", "9,1", "9,1", "9,1,9"));
 	}
 
 }
